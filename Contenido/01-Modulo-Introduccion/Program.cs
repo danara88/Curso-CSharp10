538e2374e019d13cc1fc8b8ad26d82a0db308374
Daniel Aranda <C-Daniel.Aranda@charter.com>
2022-11-04 12:08:38 -0600

    Contenido e introduccion

---
+++ Contenido/01-Modulo-Introduccion/Program.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * ¿Qué es C#?
+ * 
+ * Es un lenguage de programación compilado que cumple con las siguientes características:
+ * 
+ * 1. Multiparadigma: Permite la utilización de la programacion orienta a objetos,
+ * la programación funcional, imperativa, dirigida por eventos ...
+ * 
+ * 2. Es un lenguaje fuertemente tipado.
+ * 
+ * 3. Es modular, esto quiere decir que esta basado en clases que encapsulan parte de nuetra
+ * aplicación para tener un proyecto organizado y mantenible.
+ * 
+ * 4.Puedes dedarrollar:
+ *      - Aplicaciones móviles
+ *      - Aplicaciones de escritorio
+ *      - Aplicaciones Web
+ *      - Videojuegos
+ *      - etc
+ *
+ * 5. C# es Open Source
+ * 
+ **/
+
+/**
+ * ¿Qué es el framework .NET?
+ * 
+ * Es una plataforma de desarrollo donde podemos hacer distintos tipos de aplicaciones.
+ * 
+ * - Aplicaciones Web
+ * - Aplicaciones Móviles
+ * - Aplicaciones de Escritorio
+ * 
+ * Esta plataforma de desarrollo soporta los lenaguajes de programación C#, F# y Visual Basic
+ * 
+ **/
+
+/**
+ * Arquitectura del framework .NET
+ * 
+ * 1. Los programas de C# corren en .NET a través del Common Language Runtime (CLR).
+ * El CLR es un implementación de Microsoft, replicando el estandar Common Language
+ * Infrastructure (CLI) en donde se crea la ejecución del programa y se hacen ambientes
+ * donde lenguajes y librerías se unen y funciones a la perfección.
+ * 
+ * 2. Tu código escrito en C# se compila en archivos IL (Intermediate Language) donde se
+ * hace un ensamblado. Estos tienen una extensión .dll.
+ * 
+ * 3. Cuando ejecutas tu programa en C#, los ensamblados generados se cargan en el CLR.
+ * 
+ * 4. El CLR realiza una compilacion Just-In-Time (JIT) para convertir el código de los
+ * archivos IL a instrucciones de máquina nativas.
+ * 
+ * 5. Los archivos IL pueden interactuar con código desarrollado en F#, Visual Basic or C++
+ * 
+ **/ 
